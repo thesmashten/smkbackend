2b604ea654219a7542e168fdce511bbeae6d0247
Du Duong <du.k.duong@vanderbilt.edu>
2022-04-06 20:35:20 -0500

    tracker

---
+++ Routers/tracker.router.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose"),
+    Schema = mongoose.Schema;
+//can move this into Kid.js?
+let Tracker = new Schema({
+    goalCount: { type: Number, default: 0 },
+    threeGoalsReached : { type: Boolean, default: goalCount >= 3 },
+    firstDeposit: { type: Boolean, default: false },
+    allBanksUsed: { type: Boolean, default: false },
+    hundoBadge: { type: Boolean, default: false },
+    badges: { type: [String], default: [] },
+    fireId: { type: String, required: true, index: { unique: true } },
+})
+// check badge method
+// checkThreeGoals()
+// checkFirstDeposit()...
+//
+module.exports = mongoose.model("Tracker", Parent);
